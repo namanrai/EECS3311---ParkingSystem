7dad3e70cd42e0c8e18cf060c48484e4ee0157b0
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-03-12 22:52:18 -0400

    Compilation Binary Change

--- BookingSystem/src/Main/Main.java
+++ BookingSystem/src/Main/Main.java
@@ -7,6 +7,6 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("Hello, World!");
         User ahmad = UserFactory.createUser("Student","ahmad","ahmadzkai","123");
-        System.out.println(ahmad.getName());
+        System.out.println(ahmad.getUsername());
     }
 }
