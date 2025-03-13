3d75aa512813b2153adbaae0a918dbc9359946d3
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-03-12 22:46:06 -0400

    Making User a Interface

--- BookingSystem/src/Models/User.java
+++ BookingSystem/src/Models/User.java
@@ -1,17 +1,12 @@
 package Models;
 
-public abstract class User {
-    protected String name;
-    protected String email;
-    protected String password;
+public interface User {
+    String getEmail();
+    String getPassword();
+    //String getLicensePlate();
+    //ParkingSpot getParkingSpot();
+    String getUsername();
 
-    public User(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public String getPassword() { return password; }
+    //Card paymentInformation(String input);
+    boolean registerAccount();
 }
