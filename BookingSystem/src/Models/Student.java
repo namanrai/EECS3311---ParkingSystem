b235c6cabcc19e8d80d7cd5e7eeaadaa6e7ded71
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-03-12 22:47:13 -0400

    Updating Student

--- BookingSystem/src/Models/Student.java
+++ BookingSystem/src/Models/Student.java
@@ -1,11 +1,45 @@
 package Models;
 
-public class Student extends User {
+public class Student implements  User {
+    private String email;
+    private String password;
+//    private String licensePlate;
+//    private ParkingSpot parkingSpot;
+    private String username;
+
     public Student(String name, String email, String password) {
-        super(name, email, password);
+        this.username = name;
+        this.email = email;
+        this.password = password;
     }
 
     public double getParkingRate() {
         return 5.0; // $5 per hour
+    }
+
+    @Override
+    public String getEmail() { return email; }
+
+    @Override
+    public String getPassword() { return password; }
+
+//    @Override
+//    public String getLicensePlate() { return licensePlate; }
+
+//    @Override
+//    public ParkingSpot getParkingSpot() { return parkingSpot; }
+
+    @Override
+    public String getUsername() { return username; }
+
+//    @Override
+//    public Card paymentInformation(String input) {
+//        return new Card(input); // Example logic
+//    }
+
+    @Override
+    public boolean registerAccount() {
+        // Implement account registration logic
+        return true;
     }
 }
