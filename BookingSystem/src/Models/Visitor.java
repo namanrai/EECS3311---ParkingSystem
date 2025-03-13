6a99130768317533a4c3471e4a5488adee06756b
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-03-12 22:50:33 -0400

    Updating Visitor

--- BookingSystem/src/Models/Visitor.java
+++ BookingSystem/src/Models/Visitor.java
@@ -1,11 +1,45 @@
 package Models;
 
-public class Visitor extends User {
+public class Visitor implements  User {
+    private String email;
+    private String password;
+    //    private String licensePlate;
+//    private ParkingSpot parkingSpot;
+    private String username;
+
     public Visitor(String name, String email, String password) {
-        super(name, email, password);
+        this.username = name;
+        this.email = email;
+        this.password = password;
     }
 
     public double getParkingRate() {
         return 15.0; // $15 per hour
+    }
+
+    @Override
+    public String getEmail() { return email; }
+
+    @Override
+    public String getPassword() { return password; }
+
+//    @Override
+//    public String getLicensePlate() { return licensePlate; }
+
+//    @Override
+//    public ParkingSpot getParkingSpot() { return parkingSpot; }
+
+    @Override
+    public String getUsername() { return username; }
+
+//    @Override
+//    public Card paymentInformation(String input) {
+//        return new Card(input); // Example logic
+//    }
+
+    @Override
+    public boolean registerAccount() {
+        // Implement account registration logic
+        return true;
     }
 }
