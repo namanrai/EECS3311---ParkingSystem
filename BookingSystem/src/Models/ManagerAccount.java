24cbe6df0188d32349032b6120ac7c641df7716b
Sharath_Commit <svenug23@my.yorku.ca>
2025-03-16 21:45:45 -0400

    Worked on Manager and Super Manager

---
+++ BookingSystem/src/Models/ManagerAccount.java
@@ -0,0 +1,158 @@
+package Models;
+import java.util.Random;
+
+/**
+ * This is the class for the manager account. A manager can add, enable, and disable
+ * parking spaces and parking lots. The manager username and password are unique
+ * and are generated through random number.
+ *
+ */
+
+
+public class ManagerAccount {
+    public String username;
+    public String password;
+    private static final String userChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    public static int parkingspaces = 0;
+
+
+    /**
+     * Constructor for the class
+     */
+
+    public ManagerAccount(){
+        this.username = gernerateUsername();
+        this.password = gernerateUserPassword();
+
+    }
+
+    /**
+     * This method is used for generating the String user password,
+     * satisfying the 2nd user requirement.
+     *
+     * @return generatedPassword.toString();
+     *
+     */
+    public String gernerateUserPassword(){
+        Random randomgen = new Random();
+        StringBuilder generatedPassword = new StringBuilder();
+
+        for (int j = 0; j < 6; j++) {
+            generatedPassword.append(randomgen.nextInt(10));
+        }
+
+        return generatedPassword.toString();
+    }
+
+    /**
+     *  This method is used for generating the String user password,
+     *  satisfying the 2nd user requirement.
+     * @return generatedUser.toString();
+     */
+
+    public String gernerateUsername(){
+        int length = userChar.length();
+
+        Random randomgen = new Random();
+        StringBuilder generatedUser = new StringBuilder();
+
+        for (int j = 0; j < length; j++) {
+            int index = randomgen.nextInt(userChar.length());
+            generatedUser.append(userChar.charAt(index));
+        }
+
+        return generatedUser.toString();
+    }
+
+    /**
+     *  This method is used for adding a parking lot when required.
+     * @param parkinglot m
+     *
+     *
+     */
+
+    public void addParkinglot(ParkingLot parkinglot){
+        if (parkinglot != null){
+            parkinglot.enable();
+            System.out.println("Parking Lot" + parkinglot.getLotId() + "is enabled");
+        }
+        else{
+            System.out.println("Parking Lot is not valid");
+        }
+
+
+    }
+
+    /**
+     *  This method is responsible for disabling a parking lot
+     *  when needed by a manager.
+     * @param parkinglot m
+     *
+     */
+
+    public void disableParkinglot(ParkingLot parkinglot){
+        if (parkinglot != null){
+            parkinglot.disable();
+            System.out.println("Parking Lot" + parkinglot.getLotId() + "is disabled");
+        }
+        else{
+            System.out.println("Parking Lot is not valid");
+        }
+
+    }
+
+    /**
+     *  This method is responsible for disabling a parking sapce
+     *  when needed by a manager and reduces the count of that parking space by 1
+     * @param parkingspace m
+     *
+     */
+
+    public void disableParkingspace(ParkingSpace parkingspace){
+
+
+        if(parkingspace.getId() != null){
+            parkingspace.disable();
+            System.out.println("Parking space" + parkingspace.getId() + "is disabled");
+            parkingspaces--;
+        }
+        else{
+            System.out.println("Parking space not found");
+        }
+
+    }
+
+    /**
+     *  This method is responsible for disabling a parking sapce
+     *  when needed by a manager and increases the count of that parking space by 1
+     * @param parkingspace m
+     *
+     */
+
+    public void enableParkingspace(ParkingSpace parkingspace) {
+
+        if (parkingspaces < 100) {
+            parkingspace.enable();
+            parkingspaces++;
+            System.out.println("parking spot" + parkingspace.getId() + "is enabled");
+        }
+    }
+
+
+    public String getName() {
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ BookingSystem/src/Models/SuperManager.java
@@ -0,0 +1,43 @@
+package Models;
+
+/*
+This method uses the singleton patten, to generate just one instance
+of the SuperManager account type.
+ */
+
+
+public class SuperManager {
+
+    private static final SuperManager SuperManager1 = new SuperManager();//Early Init instead of Lazy Init
+
+   /*
+    private constructor so noby outside class can access this.
+     */
+
+    private SuperManager(){
+
+    }
+
+    /*
+    This method returns the single instance of the super manager class. Following with
+    the singleton design pattern
+     */
+    public static SuperManager getInstance(){
+        return SuperManager1;
+    }
+
+     /*
+     This method return an account. Satisfying the 2nd UserReq, which informs the Supermanager
+     can create multiple manager accounts.
+
+     @Input String role
+     @return ManagerAccountfactory.createAccount();
+      */
+
+    public ManagerAccount createManagerAccount(String role){
+        ManagerAccount ManagerAccount;
+        return ManagerAccount = new ManagerAccount();
+    }
+
+
+}
